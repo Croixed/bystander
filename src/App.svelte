0d8118371d0afb7a5f85ac76350782b7c8806bf1
croix <croix>
2023-07-19 14:05:43 -0400

    added guardian feature

--- src/App.svelte
+++ src/App.svelte
@@ -1,23 +1,25 @@
 <script>
     import { loop_guard } from "svelte/internal";
+    import { tweened } from "svelte/motion";
+    import shield from './assets/shield.svg'
 
   let questionNodes = [
     {
       id: 0,
-      question: "Hi, I'm jen \n I may be having a recurring medical event \n Please follow the prompts BEFORE calling 911. \n Will you consent to help me?",
+      question: "Hi, I'm Jen. \n I may be having a recurring medical event. \n Please follow the prompts BEFORE calling 911. \n Will you help me?",
       answers: [
         {
           answer: "Touch here for Yes",
           nextQuestion: 1
         },
         {
-          answer: "No", // goto: call 911
+          answer: " Touch here for No", // goto: call 911
           nextQuestion: 12
         }
       ]
     },
     {
-      question: "Am I breathing?",
+      question: "Is Jen breathing?",
       id: 1,
       answers: [
         {
@@ -48,11 +50,11 @@
 
 
     {
-      question: "Everything is probably ok. This is likely a regular medical event for Jen not a 911 call message.",
+      question: "This appears not to be a medical emergency for Jen.\n 911 is not the preferred response.\nPlease click next to help Jen get her preferred help.",
       id: 3,
       answers: [
         {
-          answer: "Touch here for next",
+          answer: "Next",
           nextQuestion: 4
         }
       ]
@@ -62,7 +64,7 @@
 
 
     {
-      question: "Am I awake/conscious?",
+      question: "Is Jen conscious/awake?",
       id: 4,
       answers: [
         {
@@ -83,7 +85,7 @@
 
 
     {
-      question: "am I coherent/making sense?",
+      question: "Is Jen coherent/making sense?",
       id: 5,
       answers: [
         {
@@ -99,11 +101,11 @@
 
     {
       id: 6,
-      question: "Try to communicate with me and hit next when told to do so.",
+      question: "Try to communicate with Jen and click next.",
       answers: [
         {
-          answer: "Touch here for next",
-          nextQuestion: 7
+          answer: "Next",
+          nextQuestion: 8
         }
       ]
     },
@@ -119,12 +121,12 @@
 
 
     {
-      question: "Am I Moving?",
+      question: "Is Jen moving?",
       id: 7,
       answers: [
         {
           answer: "Yes",
-          nextQuestion: 13 // seizure instructions
+          nextQuestion: 14 // seizure instructions (14 goes to the warning first, then 13 is the actual instructs)
         },
         {
           answer: "No",
@@ -134,21 +136,21 @@
 
     },
     {
-      question: "Do I seem cold?",
+      question: "Does Jen seem cold?",
       id: 8,
       answers: [
         {
-          answer: "Yes",
-          nextQuestion: 16
-        },
-        {
           answer: "No",
           nextQuestion: 9
+        },
+        {
+          answer: "Yes",
+          nextQuestion: 16
         }
       ]
     },
     {
-      question: "Do I seem warm?",
+      question: "Does Jen seem warm?",
       id: 9,
       answers: [
         {
@@ -166,14 +168,11 @@
 
 
 
-
-
-
-
     {
       id: 10,
-      question: "Jen has several breathing problems. This is normal for Jen. Please help Jen support the bottom of her ribcage and wait for the timer to end.",
-      type: 240,
+      question: "Jen has several breathing problems. \n This is normal for Jen. \n Please help Jen support the bottom of her ribcage and wait for the timer to end.",
+      // type: 240,
+      timerAnim: 5,
       answers: [
         {
           answer: "Touch here for next",
@@ -183,20 +182,22 @@
     },
     {
       id: 11,
-      question: "find ___ for additional help.",
+      question: "find Addie for additional help.",
+      guardian: true,
+      guardianText: "I live across the street at the Residence Inn. Room 524. Key should be in my jacket pocket or the pocket of whatever bag I brought today. My breathing machine is in the cooler bag on the right bedside table. Thank you!",
       answers: [
-        {
-          answer: "Touch here for details",
-          nextQuestion: 2000 // update this to later lead to her address info, and later have pin functionality? 
-        }
+        // {
+        //   answer: "Touch here for details",
+        //   nextQuestion: 2000 // update this to later lead to her address info, and later have pin functionality? 
+        // }
       ]
     },
     {
       id: 12,
-      question: "Touch here to call 911.  Please share that Jen might be having a medical event and has recurring health problems. ",
+      question: "Please share that Jen might be having a medical event and has recurring health problems. ",
       answers: [
         {
-          answer: "Touch here to call 911!",
+          answer: "Touch here to call 911",
           nextQuestion: 91111111
         }
       ]
@@ -211,25 +212,28 @@
     // 13 and 14 are seizure non timer instructions
     {
       id: 13,
-      question: "Jen has atypical absence seizures. This is normal for Jen. Do not call 911 unless she’s convulsing (not normal) or is visibly bleeding. Please remain calm. Being comforting as you might to a child is helpful. If Jen is confused, terrified, or otherwise upset, that’s okay. The seizure has passed and Jen is in recovery mode. Jen may not know herself, may be stuttering, or may be entirely nonverbal. Her brain is rebooting. Water, time, sleep is the only cure. Have her drink some water, and herd her towards one of the wellness rooms once she’s able to stutter in a few minutes. Everything’s fine. ",
+      question: " If Jen is confused, terrified, or otherwise upset, that’s okay. The seizure has passed and Jen is in recovery mode. Jen may not know herself, may be stuttering, or may be entirely nonverbal. Her brain is rebooting. Water, time, and sleep is the only cure. Have her drink some water, and herd her towards one of the wellness rooms once she’s able to stutter in a few minutes. \n\n If jen is particularly upset, play her some soothing audio.",
       answers: [
         {
-          answer: "If Jen is particularly upset, click here to play her some personally soothing audio. <insert link here>", // insert link here
+          answer: "Click here for audio", // insert link here
         },
         {
-          answer: "Does Jen look confused at communication attempts? Click here to view the sign language alphabet.",
+          answer: "Click here to view the sign language alphabet",
           nextQuestion: 14
         },
-        {
-          answer: "Have you been preselected as a guardian to help Jen get home? Click here for details", // add pin later
-          nextQuestion: 20 // update with address/hotel information later? 
-        }
       ]
 
     },
     {
+      // this was the original guardian instructions but I'm changing it to be the warning before the big text
       id: 14,
-      question: " I live across the street at the Residence Inn. Room 524. Key should be in my jacket pocket or the pocket of whatever bag I brought today. After a seizure, I’m a little frightened of things in motion, like elevators or cars. Please don’t use my last name or the word home. Please understand that I currently have no filter and will not remember much later. Please be patient.",
+      question: "Everything’s fine. Jen has atypical absence seizures. Do not call 911 unless she’s convulsing (not normal) or is visibly bleeding. This is normal for Jen. Please remain calm. Being comforting as you might to a child is helpful.",
+      answers: [
+        {
+          answer: "Next",
+          nextQuestion: 13
+        }
+      ]
     },
 
 
@@ -240,8 +244,8 @@
     // seizure timer instructions. should technically comme before 12 and 13 but I'll fix it later(?)
     {
       id: 15,
-      type: 30, // I'll probably change type to "timerMinutes" later instead
-      question: "Please wait one minute. A link to call 911 will automatically appear after one minute.  If I do not regain consciousness, or am screaming while unconscious, please call 911 and share that I have absence seizures, cerebral palsy, asthma/restrictive thoracic disorder and periodic partial paralysis.",
+      type: 60, // I'll probably change type to "timerMinutes" later instead
+      question: "Please wait one minute.\nA link to call 911 will automatically appear after one minute. \nIf Jen does not regain consciousness, or is screaming while unconscious, please call 911 and share that Jen has absence seizures,\n cerebral palsy, asthma/restrictive thoracic disorder and periodic partial paralysis.",
       answers: [
         {
           answer: "Touch here if I wake up",
@@ -257,13 +261,15 @@
     // heating instructions
     {
       id: 16,
-      question: "Jen has periodic partial paralysis. This is normal for Jen. Please help Jen get warmer, especially her hands, neck, and upper legs. It will take some time for Jen to recover; however, once warmer, she will be fully communicative.",
+      question: "This is normal for Jen. Please help Jen get warmer, especially her hands, neck, and upper legs.It will take some time for Jen to recover; however, once warmer, she will be fully communicative. ",
+      answers: []
     },
 
     // cooling instructions
     {
       id: 17,
       question: "Jen has periodic partial paralysis. This is normal for Jen. Please help Jen get cooler, especially her neck, hands, and forehead. Ice or wet paper towels help.  It will take some time for Jen to recover; however, once cooler, she will be fully communicative. ",
+      answers: []
     },
 
 
@@ -271,11 +277,11 @@
     // 911 long - used for breathing response
     {
       id: 18,
-      question: "Touch here to call 911. Please share that Jen is having a medical event and has cerebral palsy, asthma/restrictive thoracic disorder, absence seizures, and periodic partial paralysis. ",
+      question: "Please share that Jen is having a medical event and has cerebral palsy, asthma/restrictive thoracic disorder, absence seizures, and periodic partial paralysis. ",
       answers: [
         {
-          answer: "click here to call 911!",
-          nextQuestion: 91111111
+          answer: "Touch here to call 911",
+          nextQuestion: 91111111 // update this later to make it link
         }
       ]
     },
@@ -300,89 +306,6 @@
 
   ]
 
-  let actionNodes2 = [
-    {
-      id: 1,
-      question: "Please share that Jen might be having a medical event and has recurring health problems.",
-      actionNode: 2, // the numbers here are probably useless?
-      answers: [
-        {
-          answer: "Touch here to call 911",
-          nextQuestion: 2
-        },
-      ]
-    },
-    {
-      id: 2,
-      question: "Please share that Jen is having a medical event and has cerebral palsy, asthma/restrictive thoracic disorder, absence seizures, and periodic partial paralysis.",
-      actionNode: 2,
-      answers: [
-        {
-          answer: "Touch here to call 911",
-          nextQuestion: 2
-        }
-      ]
-    },
-    {
-      id: 3,
-      question: "asthma instructions placeholder ",
-      answers: [
-        {
-          answer: "Asthma placeholder button 1",
-          actionNode: 4
-        },
-        {
-          answer: "Asthma placeholder button 2",
-          actionNode: 5
-        }
-      ],
-      actionNode: 4
-    },
-    {
-      id: 4,
-      question: "Jen has periodic partial paralysis. This is normal for Jen. Please help Jen get warmer, especially her hands, neck, and upper legs. It will take some time for Jen to recover; however, once warmer, she will be fully communicative. ",
-      actionNode: 5
-    },
-    {
-      id: 5,
-      question: "Jen has periodic partial paralysis. This is normal for Jen. Please help Jen get cooler, especially her neck, hands, and forehead. Ice or wet paper towels help.  It will take some time for Jen to recover; however, once cooler, she will be fully communicative.",
-      actionNode: 6
-    },
-    {
-      id: 5,
-      question: "placeholder seizure timer instructions go here",
-      actionNode: 6
-    },
-
-    
-  ]
-
-  let actionNodes = [
-    {
-      id: 1,
-      question: "Jen has several breathing problems. This is normal for Jen. Please help Jen support the bottom of her ribcage and wait for the timer to end.",
-      type: "timed", // should I have 1 for timed 0 for not instead?
-      // or perhaps a variable called 
-      answers: [
-        {
-          answer: "click here for next",
-          nextQuestion: 2
-        }
-      ]
-    },
-    {
-      id: 2,
-      question: "find ___ for additional help.",
-      type: "timed",
-      answers: [
-        {
-          answer: "click here for details",
-          nextQuestion: 3
-        }
-      ]
-    }
-  ]
-
   let currentQuestion = questionNodes[0]
 
 
@@ -390,7 +313,7 @@
 
 
   let tempFunc = () => {
-    // if the current question is 12, 18, or 19, then tapping a button should do window.location.href="tel:911"
+    // if the current question is 12, 18, or 19, then tapping a button should dial 911
     if (currentQuestion.id === 12 || currentQuestion.id === 18 || currentQuestion.id === 19) {
       window.location.href = "tel:911"
     }
@@ -422,7 +345,7 @@
 
 
   $: {
-    if (currentQuestion.type) {
+    if (currentQuestion.id === 15) {
       console.log("this question has a type")
 
       {updateQ()}
@@ -431,7 +354,7 @@
             if (qDuringCall == currentQuestion) {
               currentQuestion = questionNodes[currentQuestion.answers[0].nextQuestion]
             }
-          }, currentQuestion.type * 1000)} 
+          }, currentQuestion.type * 1000)} // make sure to fix the multiplier later!!!! !!!! this one is for moving forward, not disabling
 
     }
   }
@@ -446,9 +369,22 @@ $: {
       isDisabled = true;
       setTimeout(() => {
         isDisabled = false;
-      }, 30000);
+      }, 10000);
     }
   }
+
+
+
+  let orig = 10
+  let timeVar = tweened(orig)
+
+  setInterval(()=> {
+    if ($timeVar >= 1 && currentQuestion.timerAnim) {
+      $timeVar--
+      console.log($timeVar)
+    }
+  }, 1000)
+
 </script>
 
 
@@ -463,16 +399,28 @@ $: {
 
 
 
+      {#if currentQuestion.timerAnim}
+        <div class="timerAnim">Next will appear at the end of the timer: <br> {Math.round($timeVar)} seconds remaining </div>
+      {/if}
 
 
-
-
-      <button class="btn-primary" on:click={tempFunc} disabled={isDisabled}>{currentQuestion.answers[0].answer}</button>
+      <!-- this is getting a little messy and I'll refactor later -->
+      {#if (!currentQuestion.timerAnim || Math.trunc($timeVar) <= 0) && currentQuestion.answers.length > 0}
+        <button class="btn-primary" on:click={tempFunc} disabled={isDisabled}>{currentQuestion.answers[0].answer}</button>
+      {/if}
       {#if currentQuestion.answers[1]}
+        {#if currentQuestion.id === 13}
+          <p class="card-text">Does Jen look confused at communication attempts?</p>
+        {/if}
         <button class="btn-primary-no" on:click={() => currentQuestion = questionNodes[currentQuestion.answers[1].nextQuestion]}>{currentQuestion.answers[1].answer}</button>
       {/if}
       {#if currentQuestion.answers[2]}
         <button class="btn-primary-no guardian" on:click={() => currentQuestion = questionNodes[currentQuestion.answers[2].nextQuestion]}>{currentQuestion.answers[2].answer}</button>
+      {/if}
+      {#if currentQuestion.answers.length === 0 || currentQuestion.id === 13}
+        <button class="guardian-btn" on:click={() => currentQuestion.question = currentQuestion.guardianText}>
+          <img alt="guardian shield logo" src={shield} />
+        </button>
       {/if}
     </div>
   </div>
@@ -488,6 +436,8 @@ $: {
 
 .card-text {
   white-space: pre-line;
+  max-width: 300px;
+  margin: 0 auto;
 }
 
 .card-body {
@@ -510,6 +460,19 @@ $: {
 .btn-primary:disabled {
   background-color: #6c757d;
   border-color: #6c757d;
+}
+
+
+.guardian-btn {
+  position: absolute;
+  bottom: -10px;
+  left: -10px;
+  background-color: transparent;
+}
+
+img {
+  width: 70px;
+  opacity: 0.12;
 }
 
 </style>
